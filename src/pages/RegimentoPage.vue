c10bf4f203a18960bf813e74514727f85250d989
marcaoteixeira <marcao.teixeira@gmail.com>
2023-08-02 21:37:46 -0300

    commit 002 02/08/2023

--- src/pages/RegimentoPage.vue
+++ src/pages/RegimentoPage.vue
@@ -67,7 +67,7 @@ import axios from "axios";
 export default defineComponent({
   created() {
     axios
-      .post("http://localhost:8686/admin/titulo/list")
+      .post("http://18.229.118.205:8686/admin/titulo/list")
       .then((res) => {
         this.titulos = res.data;
       })
@@ -75,7 +75,7 @@ export default defineComponent({
         console.log(err);
       });
     axios
-      .post("http://localhost:8686/admin/artigo/list")
+      .post("http://18.229.118.205:8686/admin/artigo/list")
       .then((res) => {
         this.artigos = res.data;
       })
@@ -83,21 +83,21 @@ export default defineComponent({
         console.log(err);
       });
     axios
-      .post("http://localhost:8686/admin/capitulo/list")
+      .post("http://18.229.118.205:8686/admin/capitulo/list")
       .then((res) => {
         this.capitulos = res.data;
       })
       .catch((err) => {
         console.log(err);
       });
-    axios.post("http://localhost:8686/admin/secao/list").then(res => {
+    axios.post("http://18.229.118.205:8686/admin/secao/list").then(res => {
       this.secoes = res.data;
       this.secoes = this.secoes.filter(c => c.id_capitulo === this.capitulo.id)
     }).catch(err => {
       console.log(err);
     });
 
-    axios.post("http://localhost:8686/admin/subsecao/list").then(res => {
+    axios.post("http://18.229.118.205:8686/admin/subsecao/list").then(res => {
       this.subsecoes = res.data;
       this.subsecoes = this.subsecoes.filter(c => c.id_secao === this.secao.id)
 
